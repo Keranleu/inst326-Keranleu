26f9490402058fca83131d4c69b20e30ad9f4606
Keranleu <keranleukeu@outlook.com>
2025-09-14 21:09:36 -0400

    week 2 update

--- library/helloworld.py
+++ library/helloworld.py
@@ -1,5 +1,28 @@
-item = 'banana'
-Item = 'Apple'
-item_name = 'Orange'
+import random
 
-print(item, Item, item_name)
+def number_guessing_game():
+    print("guess the number")
+    print("I'm thinking of a number between 1 and 100.")
+
+    # Generate a random number
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Correct! The number was {secret_number}.")
+                print(f"You guessed it in {attempts} tries!")
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    number_guessing_game()
